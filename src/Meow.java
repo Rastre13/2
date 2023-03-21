c5c47cc03899b804a19c5efd9ed2939dc8584081
Rastre <rastor13666gmail.com>
2023-03-21 22:59:13 +0300

    meow

--- src/Meow.java
+++ src/Meow.java
@@ -1,7 +1,5 @@
 public class Meow {
     public static void main(String[] args) {
         System.out.println("meow");
-        System.out.println("wow");
-        System.out.println("test2");
     }
 }
